273702f6bb90f1bd181716809dac7136d14e1ab5
TE22C-Ludwig-Langell <ludwig.langell@elev.ga.ntig.se>
2025-09-29 12:11:19 +0200

    Variable names and styling for frontend

--- Frontend/frontend/src/components/Register.vue
+++ Frontend/frontend/src/components/Register.vue
@@ -1,8 +1,9 @@
-<template>
-   <div>
+<template class="templa">
+   <div class="register">
     <h2>Register account</h2>
-    <input v-model="email" placeholder="Email">
+    <input v-model="name" placeholder="Name">
     <input v-model="password" type="password" placeholder="Password">
+    <input v-model="admin" type="checkbox" class="admincheck">Are you admin?</input>
     <button @click="register">Register</button>
     <div v-if="qr">
       <h3>Scan in Google Authenticator</h3>
@@ -14,18 +15,42 @@
 import axios from "axios";
 export default{
   data(){
-    return {email: "", password: "", qr: null};
+    return {name: "", password: "", qr: null};
   },
   methods: {
     async register(){
       const res =await
       axios.post("http://localhost:5000/api/register", {
-        email: this.email,
-        password: this.password
-
+        name: this.name,
+        password: this.password,
+        admin: this.admin
       });
       this.qr = res.data.qr;
     }
   }
 };
 </script>
+<style>
+.register{
+     display: flex;
+    flex-direction: column;
+    width: 15%;
+    background-color: darkkhaki;
+    width: 15%;
+    align-content: space-around;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+.admincheck{
+  width: 10%;
+
+
+}
+body{
+  background-color: darkkhaki;
+  width: 100%; 
+  height: auto;
+}
+
+
+</style>
